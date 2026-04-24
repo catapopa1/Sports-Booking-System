aad11578e1c7069ecda540996b58f4dac3f99ed4
Popa Catalin <catapopa.bh@gmail.com>
2026-04-24 13:43:49 +0300

    Added Environment files, Global styles, PrimeNG wiring

--- frontend/sports-booking-system/src/app/app.config.ts
+++ frontend/sports-booking-system/src/app/app.config.ts
@@ -1,11 +1,21 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: { darkModeSelector: '.dark' }
+      }
+    })
   ]
 };
